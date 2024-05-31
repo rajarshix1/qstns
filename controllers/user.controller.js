17a8543ed83bee2d8d0c475bed463c11e8c8870c
Rajarshi Mandal <rajarshim5@gmail.com>
2024-06-01 00:07:26 +0530

    Signup added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,13 +1,23 @@
-import userModel from "../models/user.model";
-
+const userModel = require('../models/user.model')
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const signUp = async (data) => {
     try {
-        let existingUser = await userModel.findOne({email: data.email })
-        if(existingUser) throw new Error(`User ${data.email} already exists`)
+        let existingUser = await userModel.findOne({email: data?.email })
+        if(existingUser) throw new Error(`User ${data?.email} already exists`)
         console.log(`user`)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(data?.password, salt);
+        const newUser = await userModel.create({
+            email: data?.email,
+            password:hashedPassword,
+            image: data?.image,
+            name: data?.name
+        })
+        newUser.password = undefined
+        return newUser
     } catch (error) {
         console.log(error)
     }   
 }
+module.exports = {signUp}
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const serverResponse = require('../helpers/serverResponse');
+const { signUp } = require('../controllers/user.controller');
 const usersRouter = express.Router();
-usersRouter.get('/', async (req, res) => {
-    serverResponse(true,"ok", "data", res, 202);
+usersRouter.post('/register', async (req, res) => {
+     serverResponse(true,"ok", await signUp(req.body), res, 202);
 });
 
 module.exports = usersRouter;
